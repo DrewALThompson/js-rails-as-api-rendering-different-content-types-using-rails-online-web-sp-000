0d84cc089c431685bf7ad699ee6740498cefe55c
Drew Thompson <drewallynthompson@gmail.com>
2021-01-28 06:13:44 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,15 @@
+<<<<<<< HEAD
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
     render json: birds
   end
 end
+=======
+class BirdsController < ApplicationController
+  def index
+    @birds = Bird.all
+    render plain: "Hello #{@birds[3].name}"
+  end
+end
+>>>>>>> 35989793fab05a3170e3d8e98004e9102b7b3c0f
